22a2bd204af17fe48163825dd5eeb743035dae05
Jonathan Urquiza <jurquiza86@hotmail.com>
2023-11-21 22:25:42 -0300

    cambiando errores

--- index.js
+++ index.js
@@ -6,4 +6,31 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 
+app.get('/', (req, res) => {
+    res.send(__dirname + './index.html')  
+})
+
+
+app.get('/admin', (req, res) => {
+    res.sendFile(__dirname + '/public/pages/admin/admin.html')
+})
+
+
+
+app.get('/item', (req, res) => {
+    res.sendFile(__dirname + '/public/pages/shop/item.html')
+})
+
+app.get('/cart', (req, res) => {
+
+    res.sendFile(__dirname + '/public/pages/shop/cart.html')
+
+})
+ 
+app.get('/shop', (req, res) => {
+
+    res.sendFile(__dirname + '/public/pages/shop/shop.html')
+})
+
+
 app.listen(PORT, () => console.log(`🚀 Servidor corriendo en: http://localhost:${PORT}`))
